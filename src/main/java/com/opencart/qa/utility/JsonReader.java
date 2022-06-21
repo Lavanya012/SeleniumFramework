d91ee2646998ce6e72511ca80d9ea24197968dfb
Lavnya Kanti Upadhyay <LavnyaKanti.Upadhyay@nortonlifelock.com>
2022-06-20 18:46:33 -0700

    resolved conflict

--- src/main/java/com/opencart/qa/utility/JsonReader.java
+++ src/main/java/com/opencart/qa/utility/JsonReader.java
@@ -3,7 +3,9 @@ package com.opencart.qa.utility;
 public class JsonReader {
     public static void main(String[] args) {
         System.out.println("reader");
-System.out.println("testing stash1");        
+
+        System.out.println("utlity branch");
+        System.out.println("utlity branch1");
     }
 
 }
